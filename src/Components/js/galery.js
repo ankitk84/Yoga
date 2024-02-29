eda55fa3701244c2d62da755d0d1d0c175ee3b47
Ankit kumar <ankitynr38@gmail.com>
2024-02-29 23:11:43 +0530

    commit

--- src/Components/js/galery.js
+++ src/Components/js/galery.js
@@ -1,11 +1,55 @@
+// import React from "react";
+// import "../css/gallery.css";
+// import {  useNavigate } from "react-router-dom";
+// const Gallery = (props) => {
+//   const navigate = useNavigate();
+//   const showImage = (image) => {
+//     navigate('/asanaimage',{state:image});
+//   };
+//   return (
+//     <div className="performance">
+//       <h1 className="title">{props.heading}</h1>
+//       <div
+//         className="gallery"
+//         style={{
+//           display: "flex",
+//           overflowX: "scroll",
+//           height: "25vh",
+//           margin: "15px",
+//           borderRadius: "10px",
+//         }}
+//       >
+//         {props.images.map((image, index) => (
+//           <img
+//             key={index}
+//             className="galleryimg"
+//             src={image.imgurl}
+//             onClick={()=>showImage(image.imgurl)}
+//             alt={`Image ${index}: ${image.description}`}
+//           />
+//         ))}
+//       </div>
+//       <iframe
+//         className="perVideo"
+//         src={props.videolink}
+//         frameborder="0"
+//         title="1"
+//       ></iframe>
+//     </div>
+//   );
+// };
+// export default Gallery;
 import React from "react";
 import "../css/gallery.css";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 const Gallery = (props) => {
   const navigate = useNavigate();
+
   const showImage = (image) => {
-    navigate('/asanaimage',{state:image});
+    navigate("/asanaimage", { state: image });
   };
+
   return (
     <div className="performance">
       <h1 className="title">{props.heading}</h1>
@@ -24,8 +68,8 @@ const Gallery = (props) => {
             key={index}
             className="galleryimg"
             src={image.imgurl}
-            onClick={()=>showImage(image.imgurl)}
-            alt={`Image ${index}`}
+            onClick={() => showImage(image.imgurl)}
+            alt={`Image ${index}: ${image.description}`}
           />
         ))}
       </div>
@@ -38,4 +82,5 @@ const Gallery = (props) => {
     </div>
   );
 };
+
 export default Gallery;
--- src/Components/js/navbar.js
+++ src/Components/js/navbar.js
@@ -1,48 +1,82 @@
-import React from 'react'
-import '../css/navbar.css'
+// import React from 'react'
+// import '../css/navbar.css'
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+// import { useNavigate } from 'react-router-dom';
+
+// export default function Navbar() {
+//   const navigate = useNavigate();
+  
+//   const setPathForOther = (name)=>{
+//     navigate(`/${name}`)
+//     // eslint-disable-next-line
+//   }
+//   return (
+    
+//       <div className="nav">
+//         <div className="left">
+//             <img className='logo' src={"https://i.ibb.co/T4wW0Hd/nitlogo.png"} alt='img' />
+//             <div className="header">
+//             <span>YOGA TEAM</span><br/>
+//             <a className='item' href='https://nitkkr.ac.in/'>National Institute of Technology, Kurukshetra</a>
+//             </div>
+//         </div>
+//         <div className="right">
+//           <ul className='listitems'>
+//             <a className='item' onClick={()=>setPathForOther("")} href="#"><li className='itemstyle'>Home</li></a>
+//             <a className='item' href="#" onClick={()=>setPathForOther('performance')} ><li  className='itemstyle'>Performances</li></a>
+//             <NavDropdown title="Yogi" id="basic-nav-dropdown" style={{marginTop:2}} >
+//               <NavDropdown.Item href="/yogi/forward"  >Forward</NavDropdown.Item>
+//               <NavDropdown.Item href="/yogi/backward"  >Backward</NavDropdown.Item>
+//               <NavDropdown.Item href="/yogi/balancing"  >Balancing</NavDropdown.Item>        
+//               <NavDropdown.Item href="/yogi/stretching"  >Streching</NavDropdown.Item>
+//               <NavDropdown.Item href="/yogi/other"  > Member</NavDropdown.Item>
+//             </NavDropdown>
+//             <a className='item' onClick={()=>setPathForOther("contact")} href="#"> <li className='itemstyle'>Contact Us</li></a>            
+//           </ul>
+      
+//         </div>
+//       </div>
+    
+//   )
+// }
+
+import React from 'react';
+import '../css/navbar.css';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
   const navigate = useNavigate();
   
-  const setPathForOther = (name)=>{
-    navigate(`/${name}`)
-    // eslint-disable-next-line
-  }
-  return (
-    
-      <div className="nav">
-        <div className="left">
-            <img className='logo' src={"https://i.ibb.co/T4wW0Hd/nitlogo.png"} alt='img' />
-            <div className="header">
-            <span>YOGA TEAM</span><br/>
-            <a className='item' href='https://nitkkr.ac.in/'>National Institute of Technology, Kurukshetra</a>
-            </div>
-        </div>
-        <div className="right">
-          <ul className='listitems'>
-            <a className='item' onClick={()=>setPathForOther("")} href="#"><li className='itemstyle'>Home</li></a>
-            <a className='item' href="#" onClick={()=>setPathForOther('performance')} ><li  className='itemstyle'>Performances</li></a>
+  const setPathForOther = (name) => {
+    navigate(`/${name}`);
+  };
 
-            <NavDropdown title="Yogi" id="basic-nav-dropdown" style={{marginTop:2}} >
-              <NavDropdown.Item href="/yogi/forward"  >Forward</NavDropdown.Item>
-              <NavDropdown.Item href="/yogi/backward"  >
-              Backward
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/yogi/balancing"  >Balancing</NavDropdown.Item>        
-              <NavDropdown.Item href="/yogi/stretching"  >
-              Streching
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/yogi/other"  >
-                Members
-              </NavDropdown.Item>
-            </NavDropdown>
-            <a className='item' onClick={()=>setPathForOther("contact")} href="#"><li className='itemstyle'>Contact Us</li></a>            
-          </ul>
-      
+  return (
+    <div className="nav">
+      <div className="left">
+        <img className='logo' src={"https://i.ibb.co/T4wW0Hd/nitlogo.png"} alt='img' />
+        <div className="header">
+          <span>YOGA TEAM</span><br/>
+          <a className='item' href='https://nitkkr.ac.in/' target="_blank" rel="noopener noreferrer">
+            National Institute of Technology, Kurukshetra
+          </a>
         </div>
       </div>
-    
-  )
+      <div className="right">
+        <ul className='listitems'>
+          <li className='itemstyle' onClick={() => setPathForOther("")}>Home</li>
+          <li className='itemstyle' onClick={() => setPathForOther('performance')}>Performances</li>
+          <NavDropdown title="Yogi" id="basic-nav-dropdown" style={{ marginTop: 2 }}>
+            <NavDropdown.Item href="/yogi/forward">Forward</NavDropdown.Item>
+            <NavDropdown.Item href="/yogi/backward">Backward</NavDropdown.Item>
+            <NavDropdown.Item href="/yogi/balancing">Balancing</NavDropdown.Item>        
+            <NavDropdown.Item href="/yogi/stretching">Stretching</NavDropdown.Item>
+            <NavDropdown.Item href="/yogi/other">Members</NavDropdown.Item>
+          </NavDropdown>
+          <li className='itemstyle' onClick={() => setPathForOther("contact")}>Contact Us</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
